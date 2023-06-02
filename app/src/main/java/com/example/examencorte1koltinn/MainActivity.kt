9883f8013b062c72000905b150d8d4aba22aa07c
BryanUziii <bryan.quintana2016@gmail.com>
2023-06-01 17:34:33 -0700

    termiando mainActivity

--- app/src/main/java/com/example/examencorte1koltinn/MainActivity.kt
+++ app/src/main/java/com/example/examencorte1koltinn/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.examencorte1koltinn
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userName: EditText = findViewById(R.id.nombreUser)
+        //val inputPass: EditText = findViewById(R.id.inputPass)
+
+        val btnEntrar: Button = findViewById(R.id.btnEntrar)
+        val btnSalir: Button = findViewById(R.id.btnSalir)
+
+
+        btnEntrar.setOnClickListener {
+            val nombreUsuario: String = userName.text.toString().trim()
+
+            if (nombreUsuario.isEmpty()) {
+                Toast.makeText(applicationContext, "Ingrese un nombre de usuario", Toast.LENGTH_SHORT).show()
+            } else {
+                val bundle = Bundle()
+                bundle.putString("nombreUsuario", nombreUsuario)
+
+                Toast.makeText(applicationContext, "Bienvenido", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this@MainActivity, RectanguloActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+        }
+
+        btnSalir.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("Confirmación")
+            builder.setMessage("¿Estás seguro de querer salir?")
+            builder.setPositiveButton("Sí") { dialog, _ ->
+                // Acciones a realizar si se selecciona "Sí"
+                finishAffinity() // Cierra todas las actividades y sale de la aplicación
+            }
+            builder.setNegativeButton("No") { dialog, _ ->
+                // Acciones a realizar si se selecciona "No"
+                dialog.dismiss() // Cierra el diálogo sin realizar ninguna acción adicional
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
     }
 }
--- app/src/main/java/com/example/examencorte1koltinn/RectanguloActivity.kt
+++ app/src/main/java/com/example/examencorte1koltinn/RectanguloActivity.kt
@@ -2,10 +2,22 @@ package com.example.examencorte1koltinn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RectanguloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rectangulo)
+
+
+
+        val textViewNombreUsuario: TextView = findViewById(R.id.lblNombreUsuario)
+
+        val extras: Bundle? = intent.extras
+
+        if (extras != null && extras.containsKey("nombreUsuario")) {
+            val nombreUsuario: String? = extras.getString("nombreUsuario")
+            textViewNombreUsuario.text = "Su nombre es: " + nombreUsuario
+        }
     }
 }
