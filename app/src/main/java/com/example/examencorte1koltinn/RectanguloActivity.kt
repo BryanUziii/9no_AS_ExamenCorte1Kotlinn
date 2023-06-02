93f2dfb3f26c4d51bc30a9ba0d47312a888a8a53
BryanUziii <bryan.quintana2016@gmail.com>
2023-06-01 17:30:58 -0700

    Creacion de las vistas

---
+++ app/src/main/java/com/example/examencorte1koltinn/RectanguloActivity.kt
@@ -0,0 +1,11 @@
+package com.example.examencorte1koltinn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RectanguloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rectangulo)
+    }
+}
